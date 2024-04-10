b998843f1dedea3d0bb0054b41a8dd04d9fdb5d2
JordanBandur <jordanbandur@hotmail.ca>
2024-04-10 12:32:27 -0700

    Make port value numeric & remove conn.end();

--- play.js
+++ play.js
@@ -4,12 +4,11 @@ const net = require("net");
 const connect = function() {
   const conn = net.createConnection({
     host: 'localhost',
-    port: '50541'
+    port: 50541
   });
   // server sends message when kicked for idling
   conn.on("data", (message) => {
     console.log(message);
-    conn.end();
   });
 
   // interpret incoming data as text
