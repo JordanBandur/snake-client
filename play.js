04f9481a0823ec6ba1f8663650ab867a3a09e3a0
JordanBandur <jordanbandur@hotmail.ca>
2024-04-10 14:09:06 -0700

    Refactor code so input.js handles input

--- play.js
+++ play.js
@@ -1,20 +1,5 @@
 const connect = require('./client');
-
-// setup interface to handle user input from stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput); // event listener for handling user input
-  return stdin;
-};
-
-const handleUserInput = function(key) { // Allows user to exit the game using ctrl + c
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
+const setupInput = require('./input');
 
 setupInput();
 connect();
