263ecb919d78f8edca3dc8b29aa4b3859ea6f160
JordanBandur <jordanbandur@hotmail.ca>
2024-04-10 13:55:00 -0700

    Add comments

--- play.js
+++ play.js
@@ -1,17 +1,16 @@
 const connect = require('./client');
 
 // setup interface to handle user input from stdin
-
 const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
+  stdin.on("data", handleUserInput); // event listener for handling user input
   return stdin;
 };
 
-const handleUserInput = function(key) {
+const handleUserInput = function(key) { // Allows user to exit the game using ctrl + c
   if (key === "\u0003") {
     process.exit();
   }
